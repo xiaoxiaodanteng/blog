bc4250db123f42e8198207cd18c6d7447a8ba0ce
qinsihui <qinsihui@paisheng.com>
2020-09-16 15:49:11 +0800

    更新

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '小小淡腾',
+  description: '小小淡腾,专注前端技术栈分享',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '前端', link: '/fontend/' },
+      { text: '小程序', link: '/wechat/' },
+      { text: '关于', link: '/about/' },
+    ]
+  }
+
+}
